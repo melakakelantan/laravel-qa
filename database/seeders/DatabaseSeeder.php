fd07ec6de49d8c93acd772cf3b17a408fd3c349c
keankiong.tan <keankiong.tan@b2be.com>
2020-09-26 16:10:56 +0800

    Prepopulate users and questions tables using model factories and seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use App\Models\Question;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        \App\Models\User::factory()->count(3)->create()->each(function($u) {
+            $u->questions()
+              ->saveMany(
+                  \App\Models\Question::factory()->count(rand(1, 5))->make()
+              );
+        });
         // User::factory(10)->create();
     }
 }
